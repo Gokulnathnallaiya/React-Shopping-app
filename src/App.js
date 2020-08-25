29ade679c33b21ea54da2a33d0723b99409e77d3
gokul nath <gokulaps1998@gmail.com>
2020-08-25 16:24:06 +0530

    products  Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-function App() {
-  return (
-    <div>
-      Gokul
-    </div>
+import  "./App.css";
+import  data from "./data.json";
+import Products from "./components/products";
+class  App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+
+      products:data.products,
+      size:"",
+      sort:"",
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main-content">
+              <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All rights Reserved
+  
+        </footer>
+  
+      
+      </div>
+      
+    );
+
     
-  );
+  }
+  
 }
 
 export default App;
